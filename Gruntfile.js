8cc4d1cd789dced327acdc0d297362bde73e4ef8
Élio Cró <elio@front.no>
2016-12-14 11:06:30 +0000

    Version 0.0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         banner: '<%= meta.banner %>',
-        mangle: false
+        mangle: true
       },
       dist: {
         src: ['<%= concat.dist.dest %>'],
--- dist/solstice.js
+++ dist/solstice.js
@@ -1,6 +1,6 @@
 /**
  * A simple Solr wrapper for AngularJS apps
- * @version v0.0.2 - 2013-11-28
+ * @version v0.0.3 - 2016-12-14
  * @link https://github.com/front/solstice
  * @author Élio Cró <elio@front.no>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -12,13 +12,13 @@
   var solr = ng.module('solstice', []);
 
   /* Solr search service */
-  solr.provider('Solstice', function () {
+  solr.provider('Solstice', [function () {
     var defEndpoint = '';
     return {
       setEndpoint: function (url) {
         defEndpoint = url;
       },
-      $get: function ($http) {
+      $get: ['$http', function ($http) {
         function Solstice(endpoint) {
           this.search = function(options) {
             var url = endpoint + '/select/';
@@ -36,12 +36,12 @@
           };
         }
         return new Solstice(defEndpoint);
-      }
+      }]
     };
-  });
+  }]);
 
   /* Solr search directive */
-  solr.directive('solrSearch', function() {
+  solr.directive('solrSearch', [function() {
     return {
       restrict: 'AE',
       transclude: true,
@@ -55,7 +55,7 @@
         fl: '@fields',
         indexUrl: '@'
       },
-      controller: function ($scope, $rootScope, Solstice) {
+      controller: ['$scope', 'Solstice', function ($scope, Solstice) {
         var searchServ = !$scope.indexUrl ? Solstice :
           Solstice.withEndpoint($scope.indexUrl);
 
@@ -72,15 +72,19 @@
         }
 
         searchServ.search(options)
-        .then(function (data) {
-          var res = data.data.response;
-          var transScope = $scope.$$nextSibling;
-          transScope.solr = $rootScope.solr || {};
-          transScope.solr.results = res.docs;
-          transScope.solr.count = res.numFound;
+        .then(function (_) {
+          var res = _.data.response;
+          var transScope = $scope.$$childHead || $scope.$$nextSibling;
+          transScope.solr = {
+            results: res.docs,
+            count: res.numFound
+          };
+        })
+        .catch(function (_) {
+          window.console.error(_.data);
         });
-      }
+      }]
     };
-  });
+  }]);
 
 })(window.angular);
